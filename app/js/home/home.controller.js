c8caa80acc6ee9a3fc7b5df0bb342c7a58088ae2
Justin Combs <combs.justin.m@gmail.com>
2015-06-29 21:13:57 -0400

    firebase and angular fire implemented, regex working for YT embed

---
+++ app/js/home/home.controller.js
@@ -0,0 +1,35 @@
+;(function (){
+
+  'use strict';
+
+  angular.module('app')
+
+    .controller('myApp', [ '$scope', '$firebaseObject',
+      function($scope, $firebaseObject){
+
+        var ref = new Firebase('radiant-heat-3085.firebaseIO.com');
+
+        var syncObject = $firebaseObject(ref);
+
+        // synchronize the object with a three-way data binding
+        // click on `index.html` above to see it used in the DOM!
+
+        syncObject.$bindTo($scope, "input");
+
+        $scope.embedify = function(link) {
+
+          var youtubeLinkPattern = /^http(s?):\/\/www\.youtu(\.be|be\.com)\/watch\?v=([-_a-zA-z0-9]+)$/;
+
+          var match = youtubeLinkPattern.exec(link);
+          if (match) {
+            $scope.embedUrl = "https://www.youtube.com/embed/" + match[3] + "?autoplay=1";
+          } else {
+            $scope.embedUrl = "";
+          }
+        }
+
+
+
+      }]);
+
+}());
--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,14 @@
-console.log('Angular Rocks', angular);
+;(function (){
+
+  'use strict';
+
+  angular.module('app', ['firebase'])
+
+    .config(function($sceProvider) {
+    // Completely disable SCE.  For demonstration purposes only!
+    // Do not use in new projects.
+    $sceProvider.enabled(false);
+  });
+
+
+}());
